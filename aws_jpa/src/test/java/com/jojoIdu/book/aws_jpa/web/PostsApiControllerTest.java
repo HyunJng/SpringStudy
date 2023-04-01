310f27a02d6ec22d707ee6f350adcf91dca5e470
HyunJng <khjung1654@gmail.com>
2023-04-02 02:55:50 +0900

    feat(등록기능 구현): JPA를 이용한 등록기능 구현
    PostsApiController, PostsService, PostsSaveRequestDto를 통해 Create기능을 구현하고
    Test까지 실행

---
+++ aws_jpa/src/test/java/com/jojoIdu/book/aws_jpa/web/PostsApiControllerTest.java
@@ -0,0 +1,64 @@
+package com.jojoIdu.book.aws_jpa.web;
+
+import com.jojoIdu.book.aws_jpa.domain.posts.Posts;
+import com.jojoIdu.book.aws_jpa.domain.posts.PostsRepository;
+import com.jojoIdu.book.aws_jpa.web.dto.PostsSaveRequestDto;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // 호스트가 사용하지 않는 랜덤 포트에서 테스트를 하겠다는 의미
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void tearDown() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() {
+        // given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .postForEntity(url, requestDto, Long.class); // url(PostsApiController)에 RequestDto를 담아 Long.class로 받아옴
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
