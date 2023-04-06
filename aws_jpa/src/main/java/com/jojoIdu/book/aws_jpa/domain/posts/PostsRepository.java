e88ade8b8be3c1013cdbcb0447a27de6a637fe2b
HyunJng <khjung1654@gmail.com>
2023-04-07 04:52:12 +0900

    Refeact: jpa관련 repository 새로 생성

--- aws_jpa/src/main/java/com/jojoIdu/book/aws_jpa/domain/posts/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jojoIdu.book.aws_jpa.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
-
