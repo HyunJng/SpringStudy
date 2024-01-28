5b79f15c5f7e07f9ed2491afa3d7ac939c6aec8c
HyunJng <khjung1654@gmail.com>
2024-01-28 20:20:27 +0900

    김영한 스프링 DB - JDBC이해

---
+++ spring_db_1/src/test/java/com/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,40 @@
+package com.hello.jdbc.repository;
+
+import com.hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    public void crud() throws Exception {
+        // save
+        Member member = new Member("memberV4", 10000);
+        repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member); // @Data에서 equals, hashCode도 만들어주기 때문에 검사 가능
+
+        // update
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
